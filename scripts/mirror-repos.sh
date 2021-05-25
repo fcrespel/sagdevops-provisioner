0d77d51dc05a547e3aa6558790e6420dbfe6c0c2
Fabien Crespel <fabien@crespel.net>
2021-05-25 19:01:25 +0200

    Add mirror timeout config

--- scripts/mirror-repos.sh
+++ scripts/mirror-repos.sh
@@ -7,10 +7,10 @@ set -e
 NODES=local SAG_HOME=$CC_HOME $CC_HOME/provision.sh
 
 # Add products mirror
-sagcc add repository products mirror name=products-mirror sourceRepos=products artifacts=$MIRROR_PRODUCTS_ARTIFACTS platforms=LNXAMD64 --sync-job -c 10 -w 1800 -e DONE
+sagcc add repository products mirror name=products-mirror sourceRepos=products artifacts=$MIRROR_PRODUCTS_ARTIFACTS platforms=LNXAMD64 --sync-job -c 10 -w $MIRROR_TIMEOUT -e DONE
 
 # Add fixes mirror
-sagcc add repository fixes mirror name=mirror sourceRepos=fixes productRepos=products-mirror artifacts=$MIRROR_FIXES_ARTIFACTS --sync-job -c 10 -w 1800 -e DONE
+sagcc add repository fixes mirror name=mirror sourceRepos=fixes productRepos=products-mirror artifacts=$MIRROR_FIXES_ARTIFACTS --sync-job -c 10 -w $MIRROR_TIMEOUT -e DONE
 
 # Shutdown CCE/SPM
 $CC_HOME/profiles/SPM/bin/shutdown.sh
